e6ad30ce30a72fd435ea3ee55ca56d08d07e594d
prosild <35754074+prosild@users.noreply.github.com>
2018-06-27 15:56:13 +0900

    6월 27일 SQL 2일차 문자함수 3시50분까지

--- sql_day02.sql
+++ sql_day02.sql
@@ -171,3 +171,472 @@ SELECT '안녕하세요. ' || e.ENAME || '씨, 당신의 사번은 ' || e.EMPNO 
 안녕하세요. J%JONES씨, 당신의 사번은 7777입니다.
 ---------------------------------------------------*/
 
+-- (6) 연산자 : 6. 집합연산자
+--  첫번째 쿼리
+SELECT *
+  FROM dept d
+;
+
+--  두번째 쿼리 : 부서번호가 10번인 부서정보만 조회
+SELECT *
+  FROM dept d
+ WHERE d.DEPTNO = 10
+;
+
+--  1) UNION ALL : 두 집합의 중복데이터 허용하여 합집합
+SELECT *
+  FROM dept d
+ UNION ALL
+SELECT *
+  FROM dept d
+ WHERE d.DEPTNO = 10
+;
+
+--  2) UNION : 중복을 제거한 합집합
+SELECT *
+  FROM dept d
+ UNION
+SELECT *
+  FROM dept d
+ WHERE d.DEPTNO = 10
+;
+
+--  3) INTERSECT : 중복된 데이터만 남김 (교집합)
+SELECT *
+  FROM dept d
+INTERSECT
+SELECT *
+  FROM dept d
+ WHERE d.DEPTNO = 10
+;
+
+--  4) MINUS : 첫번째 쿼리 실행 결과에서 두번째 쿼리 실행결과를 뺀 차집합
+SELECT *
+  FROM dept d
+ MINUS
+SELECT *
+  FROM dept d
+ WHERE d.DEPTNO = 10
+;
+
+-- 주의 ! : 각 쿼리 조회 결과의 컬럼 개수, 데이터 타입이 서로 일치해야 함
+SELECT *              -- 첫번째 쿼리 조회 컬럼 개수는 3
+  FROM dept d
+ UNION ALL
+SELECT d.DEPTNO       -- 두번째 쿼리 조회 컬럼 개수는 2
+     , d.DNAME
+  FROM dept d
+ WHERE d.DEPTNO = 10
+;
+
+-- ORA-01789: query block has incorrect number of result columns
+
+SELECT d.DNAME        -- 문자형 데이터
+     , d.DEPTNO       -- 숫자형 데이터
+  FROM dept d
+ UNION ALL
+SELECT d.DEPTNO       -- 숫자형 데이터
+     , d.DNAME        -- 문자형 데이터
+  FROM dept d
+ WHERE d.DEPTNO = 10
+;
+
+-- ORA-01790: expression must have same datatype as corresponding expression
+
+--  서로 다른 테이블에서 조회한 결과를 집합연산 가능
+--  첫번째 쿼리 : emp 테이블에서 조회
+SELECT e.EMPNO   -- 숫자
+     , e.ENAME   -- 문자
+     , e.JOB     -- 문자
+  FROM emp e
+;
+
+--  두번째 쿼리 : dept 테이블에서 조회
+SELECT d.DEPTNO  -- 숫자
+     , d.DNAME   -- 문자
+     , d.LOC     -- 문자
+  FROM dept d
+;
+
+--  서로 다른 테이블의 조회 내용을 UNION
+SELECT e.EMPNO   -- 숫자
+     , e.ENAME   -- 문자
+     , e.JOB     -- 문자
+  FROM emp e
+ UNION
+SELECT d.DEPTNO  -- 숫자
+     , d.DNAME   -- 문자
+     , d.LOC     -- 문자
+  FROM dept d
+;
+
+/*------------------------
+10	    ACCOUNTING	NEW YORK
+20      RESEARCH	DALLAS
+30	    SALES	CHICAGO
+40	    OPERATIONS	BOSTON
+7369	SMITH	CLERK
+7499	ALLEN	SALESMAN
+7521	WARD	SALESMAN
+7566	JONES	MANAGER
+7654	MARTIN	SALESMAN
+7698	BLAKE	MANAGER
+7777	J%JONES	CLERK
+7782	CLARK	MANAGER
+7839	KING	PRESIDENT
+7844	TURNER	SALESMAN
+7900	JAMES	CLERK
+7902	FORD	ANALYST
+7934	MILLER	CLERK
+8888	J	CLERK
+9999	J_JUNE	CLERK
+--------------------------*/
+
+-- 서로 다른 테이블의 조회 내용을 MINUS
+SELECT e.EMPNO   -- 숫자
+     , e.ENAME   -- 문자
+     , e.JOB     -- 문자
+  FROM emp e
+ MINUS
+SELECT d.DEPTNO  -- 숫자
+     , d.DNAME   -- 문자
+     , d.LOC     -- 문자
+  FROM dept d
+;
+
+/*-------------------------
+7369	SMITH	CLERK
+7499	ALLEN	SALESMAN
+7521	WARD	SALESMAN
+7566	JONES	MANAGER
+7654	MARTIN	SALESMAN
+7698	BLAKE	MANAGER
+7777	J%JONES	CLERK
+7782	CLARK	MANAGER
+7839	KING	PRESIDENT
+7844	TURNER	SALESMAN
+7900	JAMES	CLERK
+7902	FORD	ANALYST
+7934	MILLER	CLERK
+8888	J	CLERK
+9999	J_JUNE	CLERK
+------------------------*/
+
+-- 서로 다른 테이블의 조회 내용을 INTERSECT
+SELECT e.EMPNO   -- 숫자
+     , e.ENAME   -- 문자
+     , e.JOB     -- 문자
+  FROM emp e
+ INTERSECT
+SELECT d.DEPTNO  -- 숫자
+     , d.DNAME   -- 문자
+     , d.LOC     -- 문자
+  FROM dept d
+;
+
+-- 조회 결과 없음 : 
+-- 인출된 모든 행 : 0
+-- no rows selected
+
+-- (6) 연산자 : 7. 연산자 우선순위
+/*
+ 주어진 조건 3가지
+ 1. mgr = 7698
+ 2. job = 'CLERK'
+ 3. sal > 1300
+*/
+
+--  1) 매니저가 7698번이며, 직무는 CLERK 이거나
+--     급여는 1300이 넘는 조건을 만족하는 직원의 정보를 조회
+SELECT e.EMPNO
+     , e.ENAME
+     , e.JOB
+     , e.SAL
+     , e.MGR
+  FROM emp e
+ WHERE e.MGR = 7698
+   AND e.JOB = 'CLERK'
+    OR e.SAL > 1300
+;
+
+/*--------------------------------------
+7499	ALLEN	SALESMAN	1600	7698
+7566	JONES	MANAGER	    2975	7839
+7698	BLAKE	MANAGER	    2850	7839
+7782	CLARK	MANAGER	    2450	7839
+7839	KING	PRESIDENT	5000	
+7844	TURNER	SALESMAN	1500	7698
+7900	JAMES	CLERK	    950	    7698
+7902	FORD	ANALYST	    3000	7566
+------------------------------------------*/
+
+--  2) 매니저가 7698번인 직원중에서
+--     직무가 CLERK이거나 급여가 1300이 넘는 조건을 만족하는 직원 정보
+SELECT e.EMPNO
+     , e.ENAME
+     , e.JOB
+     , e.SAL
+     , e.MGR
+  FROM emp e
+ WHERE e.MGR = 7698
+   AND (e.JOB = 'CLERK' OR e.SAL > 1300)
+;
+
+/*-----------------------------------=-------
+7499	ALLEN	SALESMAN	1600	7698
+7844	TURNER	SALESMAN	1500	7698
+7900	JAMES	CLERK	    950	    7698
+-------------------------------------------*/
+
+--  3) 직무가 CLERK이거나
+--     급여가 1300이 넘으면서 매니저가 7698인 직원의 정보 조회
+SELECT e.EMPNO
+     , e.ENAME
+     , e.JOB
+     , e.SAL
+     , e.MGR
+  FROM emp e
+ WHERE JOB = 'CLERK'
+    OR (e.SAL > 1300 AND e.MGR = 7698)
+;
+
+/*-----------------------------------------
+9999	J_JUNE	CLERK	    500	
+7369	SMITH	CLERK	    800	    7902
+7499	ALLEN	SALESMAN	1600	7698
+7844	TURNER	SALESMAN	1500	7698
+7900	JAMES	CLERK	    950	    7698
+7934	MILLER	CLERK	    1300	7782
+8888	J	    CLERK	    400	
+7777	J%JONES	CLERK	    300	
+------------------------------------------*/
+
+--  AND 연산자의 우선순위는 자동으로 OR보다 높기 때문에
+--  두번째처럼 괄호를 사용하지 않아도 수행 결과는 같아짐
+
+SELECT e.EMPNO
+     , e.ENAME
+     , e.JOB
+     , e.SAL
+     , e.MGR
+  FROM emp e
+ WHERE JOB = 'CLERK'
+    OR e.SAL > 1300 
+   AND e.MGR = 7698
+;
+
+/*-------------------------------------------
+9999	J_JUNE	CLERK	    500	
+7369	SMITH	CLERK	    800	    7902
+7499	ALLEN	SALESMAN	1600	7698
+7844	TURNER	SALESMAN	1500	7698
+7900	JAMES	CLERK	    950	    7698
+7934	MILLER	CLERK	    1300	7782
+8888	J	    CLERK	    400	
+7777	J%JONES	CLERK	    300	
+--------------------------------------------*/
+
+-- 6. 함수
+--  (2) dual 테이블 : 1행 1열로 구성된 시스템 테이블
+DESC dual; --> 문자데이터 1칸으로 구성된 dummy 컬럼을 가진 테이블
+
+SELECT *     --> dummy 컬럼에 X값이 하나 들어있음을 확인 할 수 있다.
+  FROM dual
+;
+
+--  dual 테이블을 사용하여 날짜 조회
+SELECT sysdate
+  FROM dual
+;
+
+--  (3) 단일행 함수
+--   1) 숫자함수 : 1. MOD(m, n) : m을 n으로 나눈 나머지 계산 함수
+SELECT mod(10, 3) as "result"
+  FROM dual
+;
+
+SELECT mod(10, 3) as "result"
+  FROM emp
+;
+
+SELECT mod(10, 3) as "result"
+  FROM dept
+;
+
+--  각 사원의 급여를 3으로 나눈 나머지를 조회
+SELECT e.EMPNO
+     , e.ENAME
+     , mod(e.SAL, 3) as "RESULT"
+  FROM emp e
+;
+
+/*---------------------
+EMPNO,  ENAME,  RESULT
+-----------------------
+9999	J_JUNE	2
+7369	SMITH	2
+7499	ALLEN	1
+7521	WARD	2
+7566	JONES	2
+7654	MARTIN	2
+7698	BLAKE	0
+7782	CLARK	2
+7839	KING	2
+7844	TURNER	0
+7900	JAMES	2
+7902	FORD	0
+7934	MILLER	1
+8888	J	    1
+7777	J%JONES	0
+-------------------*/
+
+--  단일행 함수는 테이블 1행당 1번씩 적용
+
+--   2. ROUND(m, n) : 실수 , m을 소수점 n + 1 자리에서 반올림 한 결과를 계산
+SELECT round(1234.56, 1) FROM dual; --1234.6
+SELECT round(1234.56, 0) FROM dual; --1235
+SELECT round(1234.46, 0) FROM dual; --1234
+--      ROUND(m) : n값을 생략하면 소수점 이하 첫째자리 반올림 바로 수행
+--                 즉, n값을 0으로 수행함
+SELECT round(1234.46) FROM dual; -- 1234
+SELECT round(1234.56) FROM dual; -- 1235
+
+--  3. TRUNC(m, n) : 실수 m을 n에서 지정한 자리 이하 소수점 버림
+SELECT trunc(1234.56, 1) FROM dual; -- 1234.5
+SELECT trunc(1234.56, 0) FROM dual; -- 1234
+--     TRUNC(m)    : n을 생략하면 0으로 수행
+SELECT trunc(1234.56) FROM dual; -- 1234
+
+-- 4. CEIL(n) : 입력된 실수 n에서 같거나 가장 큰 가까운 정수
+SELECT ceil(1234.56) FROM dual;  -- 1235
+SELECT ceil(1234) FROM dual;     -- 1234
+SELECT ceil(1234.001) FROM dual; -- 1235
+
+-- 5. FLOOR(n) : 입력된 실수 n에서 같거나 가장 가까운 작은 정수
+SELECT FLOOR(1234.56) FROM dual;  -- 1234
+SELECT FLOOR(1234) FROM dual;     -- 1234
+SELECT FLOOR(1235.56) FROM dual;   -- 1235
+
+-- 6. WIDTH_BUCKET(expr, min, max, buckets)
+-- : min, max 값 사이를 buckets 개수만큼의 구간으로 나누고
+--   expr이 출력하는 값이 어느 구간인지 위치를 숫자로 구해줌
+
+--  급여범위를 0 ~ 5000으로 잡고, 5개의 구간으로 나누어서
+--  각 직원의 급여가 어느 구간에 해당하는지 보고서를 출력해보자.
+SELECT e.EMPNO
+     , e.ENAME
+     , e.SAL
+     , width_bucket(e.SAL, 0, 5000, 5) as "급여 구간"
+  FROM emp e
+ ORDER BY "급여 구간" DESC
+;
+
+/*--------------------------------
+EMPNO,  ENAME,  SAL,    급여 구간
+----------------------------------
+7839	KING	5000	6
+7902	FORD	3000	4
+7566	JONES	2975	3
+7782	CLARK	2450	3
+7698	BLAKE	2850	3
+7654	MARTIN	1250	2
+7521	WARD	1250	2
+7844	TURNER	1500	2
+7499	ALLEN	1600	2
+7934	MILLER	1300	2
+8888	J	    400 	1
+7900	JAMES	950	    1
+7777	J%JONES	300	    1
+9999	J_JUNE	500	    1
+7369	SMITH	800	    1
+--------------------------------*/
+
+-- 2) 문자 함수
+--  1. INITCAP(str) : str의 첫 글자를 대문자화 (영문인 경우)
+SELECT initcap('the soap') FROM dual; -- The Soap
+SELECT initcap('안녕하세요, 하하') FROM dual; -- 안녕하세요, 하하
+
+--  2. LOWER(str) : str을 소문자화 (영문인 경우)
+SELECT lower('MR. SCOTT MCMILLAN') as "소문자로 변경" FROM dual;
+
+--  3. UPPER(str) : str을 대문자화 (영문인 경우)
+SELECT upper('lee') as "성을 대문자로 변경" FROM dual;
+SELECT upper('sql is cooooooooooool~~!!') as "씐나!" FROM dual;
+
+--  4. LENGTH(str), LENGTHB(str) : str의 글자길이 계산, 글자의 byte계산
+SELECT LENGTH('hello, sql') as "글자 길이" FROM dual;
+SELECT 'hello, sql의 글자 길이는 ' || LENGTH('hello, sql') || '입니다.' as "글자 길이"
+  FROM dual
+;
+
+--  oracle에서 한글은 3byte로 계산
+SELECT lengthb('hello, sql') as "글자 byte" FROM dual;
+SELECT lengthb('오라클') as "글자 byte" FROM dual;
+
+--  5. CONCAT(str1, str2) : str1, str2 문자열을 접합, || 연산자와 동일
+SELECT concat('안녕하세요, ', 'SQL') FROM dual;
+SELECT '안녕하세요, ' || 'SQL' FROM dual;
+
+--  6. SUBSTR(str, i, n) : str에서 i번째 위치에서 n개의 글자를 추출
+--     SQL에서 문자열 인덱스를 나타내는 i는 1부터 시작에 주의함!
+SELECT substr('sql is cooooooooooool~~!!', 3, 4) FROM dual;
+--     SUBSTR(str, i) : i번째 위치에 문자열 끝까지 추출
+SELECT substr('sql is cooooooooooool~~!!', 3) FROM dual;
+
+--  7. INSTR(str1, str2) : 2번째 문자열이 1번째 문자열 어디에 위차하는가 등장하는 위치를 계산
+SELECT instr('sql is cooooooooooool~~!!', 'is') FROM dual;
+SELECT instr('sql is cooooooooooool~~!!', 'ia') FROM dual;
+-- 2번째 문장이 등장하지 않으면 0으로 계산
+
+--  8. LPAD, RPAD(str, n, c)
+--     : 입력된 str에 대해서, 전체 글자의 자릿수를 n으로 잡고
+--       남는 공간에 왼쪽, 혹은 오른쪽으로 c의 문자를 채워넣는다.
+SELECT LPAD('sql is cooool', 20, '!') FROM dual;
+SELECT RPAD('sql is cooool', 25, '!') FROM dual;
+
+--  9. LTRIM, RTRIM, TRIM : 입력된 문자열의 왼쪽, 오른쪽, 양쪽 공백 제거
+SELECT '>' || ltrim('     sql is cool      ') || '<' FROM dual;
+SELECT '>' || rtrim('     sql is cool      ') || '<' FROM dual;
+SELECT '>' || trim('     sql is cool      ') || '<' FROM dual;
+
+-- 10. NVL(expr1, expr2), NVL2(expr1, expr2, expr3), NULLIF(expr1, expr2)
+-- nvl(expr1, expr2) : 첫번째 식의 값이 NULL이면 두번째 식으로 대체하여 출력
+-- mgr가 배정 안 된 직원의 경우 '매니저 없음' 으로 변경해서 출력
+SELECT e.EMPNO
+     , e.ENAME
+     , nvl(e.MGR, '매니저 없음')  -- mgr 숫자 데이터, 변경 출력이 문자
+  FROM emp e                      -- 타입 불일치로 실행이 안됨
+;
+----------------
+SELECT e.EMPNO
+     , e.ENAME
+     , nvl(e.MGR, 0)
+  FROM emp e
+;
+----------------
+SELECT e.EMPNO
+     , e.ENAME
+     , nvl(e.MGR || '', '매니저 없음') -- || 결합연산자로 '' 빈문자를 붙여서 형변환
+  FROM emp e
+;
+
+-- nvl2(expr1, expr2, expr3) : 첫번째 식의 값이 NOT NULL이면 두번째 식의 값으로 대체하여 출력
+--                                              NULL이면 세번째 식의 값으로 대체하여 출력
+SELECT e.EMPNO
+     , e.ENAME
+     , nvl2(e.MGR, '매니저 있음', '매니저 없음')
+  FROM emp e
+;
+
+-- nullif(expr1, expr2) : 첫번째 식, 두번째 식의 값이 동일하면 NULL을 출력
+--                        식의 값이 다르면 첫번째 식의 값을 출력
+SELECT nullif('AAA', 'bbb')
+  FROM dual
+;
+
+SELECT nullif('AAA', 'AAA')
+  FROM dual
+;
+-- 조회된 결과 1행이 NULL인 결과를 얻게 됨
+-- 1행이라도 NULL이 조회된 결과는 인출된 모든 행 : 0과는 상태가 다름!
+
