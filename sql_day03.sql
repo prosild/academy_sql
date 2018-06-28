e32e501b738b67d5a5f6488a5d04c366f3fc5250
GIL SU PARK <prosild@naver.com>
2018-06-28 09:55:14 +0900

    6월 27일 SQL 3일차 CASE ~ END 까지 오전 9시55분까지

--- sql_day03.sql
+++ sql_day03.sql
@@ -41,13 +41,54 @@ SELECT e.EMPNO
 --  CASE 결과에 숫자 통화 패턴 씌우기 : $기호
 SELECT e.EMPNO
      , e.ENAME
-     , e.JOB
-     , CASE WHEN e.JOB = 'CLERK'     THEN e.SAL * 0.05
-            WHEN e.JOB = 'SALESMAN'  THEN e.SAL * 0.04
-            WHEN e.JOB = 'MANAGER'   THEN e.SAL * 0.037
-            WHEN e.JOB = 'ANALYST'   THEN e.SAL * 0.03
-            WHEN e.JOB = 'PRESIDENT' THEN e.SAL * 0.015
-            ELSE 10
-        END as "경조사 지원금"
+     , nvl(e.JOB, '미지정') as "JOB"
+     , TO_CHAR(CASE WHEN e.JOB = 'CLERK'     THEN e.SAL * 0.05
+                    WHEN e.JOB = 'SALESMAN'  THEN e.SAL * 0.04
+                    WHEN e.JOB = 'MANAGER'   THEN e.SAL * 0.037
+                    WHEN e.JOB = 'ANALYST'   THEN e.SAL * 0.03
+                    WHEN e.JOB = 'PRESIDENT' THEN e.SAL * 0.015
+                    ELSE 10
+                END, '$9,999.99') as "경조사 지원금"
   FROM emp e
+;
+
+/* SALGRADE 테이블의 내용 : 이 회사의 급여 등급 기준 값
+GRADE, LOSAL, HISAL
+---------------------
+1	     700 	1200
+2	    1201	1400
+3	    1401	2000
+4	    2001	3000
+5	    3001	9999
+--------------------*/
+
+--  제공되는 급여 등급을 바탕으로 각 직원들의 급여 등급을 구해보자
+--  CASE를 사용하여
+SELECT e.EMPNO
+     , e.ENAME
+     , e.SAL
+     , CASE WHEN e.SAL >= 700 AND e.SAL <= 1200 THEN 1
+            WHEN e.SAL > 1200 AND e.SAL <= 1400 THEN 2
+            WHEN e.SAL > 1400 AND e.SAL <= 2000 THEN 3
+            WHEN e.SAL > 2000 AND e.SAL <= 3000 THEN 4
+            WHEN e.SAL > 3000 AND e.SAL <= 9999 THEN 5
+            ELSE 0
+        END as "급여 등급"
+  FROM emp e
+ ORDER BY "급여 등급" DESC
+;
+
+--  WHEN안의 구문을 BETWEEN ~ AND ~으로 변경하여 작성
+SELECT e.EMPNO
+     , e.ENAME
+     , e.SAL
+     , CASE WHEN e.SAL BETWEEN  700 AND 1200 THEN 1
+            WHEN e.SAL BETWEEN 1201 AND 1400 THEN 2
+            WHEN e.SAL BETWEEN 1401 AND 2000 THEN 3
+            WHEN e.SAL BETWEEN 2001 AND 3000 THEN 4
+            WHEN e.SAL BETWEEN 3001 AND 9999 THEN 5
+            ELSE 0
+        END as "급여 등급"
+  FROM emp e
+ ORDER BY "급여 등급" DESC
 ;
