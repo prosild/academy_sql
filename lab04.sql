9bd36a445733bfab7ea68160c93f454705e58858
GIL SU PARK <prosild@naver.com>
2018-07-01 13:57:24 +0900

    7월 1일 SQL 4일차 과제 마무리 오후 1시 57분

--- lab04.sql
+++ lab04.sql
@@ -77,24 +77,24 @@ SELECT e1.EMPNO as "사번"
   FROM emp e1
 ;
 /*----------------------
-사번,   이름,   상사이름
+사번,   이름,     상사이름
 ------------------------
-6666	JJ	    -
-9999	J_JUNE	-
-7369	SMITH	FORD
-7499	ALLEN	BLAKE
-7521	WARD	BLAKE
-7566	JONES	KING
-7654	MARTIN	BLAKE
-7698	BLAKE	KING
-7782	CLARK	KING
-7839	KING	-
-7844	TURNER	BLAKE
-7900	JAMES	BLAKE
-7902	FORD	JONES
-7934	MILLER	CLARK
-8888	J	    -
-7777	J%JONES	-
+6666	  JJ	      -
+9999	  J_JUNE	  -
+7369	  SMITH   	FORD
+7499	  ALLEN   	BLAKE
+7521	  WARD	    BLAKE
+7566	  JONES   	KING
+7654	  MARTIN  	BLAKE
+7698	  BLAKE   	KING
+7782	  CLARK	    KING
+7839	  KING	    -
+7844	  TURNER   	BLAKE
+7900	  JAMES   	BLAKE
+7902	  FORD	    JONES
+7934	  MILLER  	CLARK
+8888	  J	        -
+7777	  J%JONES 	-
 -----------------------*/
 
 -- 7)
@@ -115,24 +115,24 @@ SELECT e.EMPNO
 ;
 
 /*----------------------------------------------------------
-EMPNO, ENAME, JOB, SAL, DNAME, LOC
+EMPNO, ENAME, JOB,        SAL,    DNAME,      LOC
 ------------------------------------------------------------
-6666	JJ		2800		
-9999	J_JUNE	CLERK	    500		
-7369	SMITH	CLERK	    800	    RESEARCH	DALLAS
-7499	ALLEN	SALESMAN	1600	SALES	    CHICAGO
-7521	WARD	SALESMAN	1250	SALES	    CHICAGO
-7566	JONES	MANAGER	    2975	RESEARCH	DALLAS
-7654	MARTIN	SALESMAN	1250	SALES	    CHICAGO
-7698	BLAKE	MANAGER	    2850	SALES	    CHICAGO
-7782	CLARK	MANAGER	    2450	ACCOUNTING	NEW YORK
-7839	KING	PRESIDENT	5000	ACCOUNTING	NEW YORK
-7844	TURNER	SALESMAN	1500	SALES	    CHICAGO
-7900	JAMES	CLERK	    950	    SALES	    CHICAGO
-7902	FORD	ANALYST	    3000	RESEARCH	DALLAS
-7934	MILLER	CLERK	    1300	ACCOUNTING	NEW YORK
-8888	J	    CLERK	    400		
-7777	J%JONES	CLERK	    300		
+6666	JJ		              2800		
+9999	J_JUNE	CLERK	      500		
+7369	SMITH 	CLERK	      800	    RESEARCH	  DALLAS
+7499	ALLEN	  SALESMAN	  1600	  SALES	      CHICAGO
+7521	WARD	  SALESMAN	  1250	  SALES	      CHICAGO
+7566	JONES	  MANAGER	    2975	  RESEARCH	  DALLAS
+7654	MARTIN	SALESMAN	  1250	  SALES	      CHICAGO
+7698	BLAKE	  MANAGER	    2850	  SALES	      CHICAGO
+7782	CLARK	  MANAGER	    2450	  ACCOUNTING	NEW YORK
+7839	KING	  PRESIDENT	  5000	  ACCOUNTING	NEW YORK
+7844	TURNER	SALESMAN	  1500	  SALES	      CHICAGO
+7900	JAMES	  CLERK	      950	    SALES	      CHICAGO
+7902	FORD	  ANALYST	    3000	  RESEARCH	  DALLAS
+7934	MILLER	CLERK	      1300	  ACCOUNTING	NEW YORK
+8888	J	      CLERK	      400		
+7777	J%JONES	CLERK	      300		
 -----------------------------------------------------------*/
 
 -- DDL
@@ -160,3 +160,164 @@ BIRTHYEAR          NUMBER(4)
 REGDATE            DATE         
 ADDRESS            VARCHAR2(30)
 ------------------------------*/
+
+-- 3)
+CREATE TABLE new_cust
+   AS SELECT *
+        FROM customer
+       WHERE 1 = 2
+;
+/*-----------------------------------
+Table NEW_CUST이(가) 생성되었습니다.
+------------------------------------*/
+
+-- 4)
+DESC new_cust;
+/*-----------------------------
+이름        널        유형           
+--------- -------- ------------ 
+USERID             VARCHAR2(4)  
+NAME      NOT NULL VARCHAR2(30) 
+BIRTHYEAR          NUMBER(4)    
+REGDATE            DATE         
+ADDRESS            VARCHAR2(30)
+-------------------------------*/
+
+-- 5)
+CREATE TABLE salesman
+    AS SELECT *
+         FROM emp e
+        WHERE e.JOB = 'SALESMAN'
+;
+/*----------------------------------
+Table SALESMAN이(가) 생성되었습니다.
+-----------------------------------*/
+
+-- 6)
+SELECT *
+  FROM salesman
+;
+/*------------------------------------------------------------
+EMPNO, ENAME, JOB,      MGR,  HIREDATE, SAL,  COMM, DEPTNO
+--------------------------------------------------------------
+7499	 ALLEN	SALESMAN	7698	81/02/20	1600	300	  30
+7521	 WARD	  SALESMAN	7698	81/02/22	1250	500	  30
+7654	 MARTIN	SALESMAN	7698	81/09/28	1250	1400	30
+7844	 TURNER	SALESMAN	7698	81/09/08	1500	0	    30
+------------------------------------------------------------*/
+
+--7)
+ALTER TABLE customer ADD
+(  phone VARCHAR2(11)
+ , grade VARCHAR2(30)
+ , CONSTRAINT ck_customer_grade CHECK (grade IN ('VIP', 'GOLD', 'SILVER'))
+);
+/*------------------------------------
+Table CUSTOMER이(가) 변경되었습니다.
+-------------------------------------*/
+DESC customer;
+/*-----------------------------
+이름        널        유형           
+--------- -------- ------------ 
+USERID    NOT NULL VARCHAR2(4)  
+NAME      NOT NULL VARCHAR2(30) 
+BIRTHYEAR          NUMBER(4)    
+REGDATE            DATE         
+ADDRESS            VARCHAR2(30) 
+PHONE              VARCHAR2(11) 
+GRADE              VARCHAR2(30)
+-------------------------------*/
+
+-- 8)
+ALTER TABLE customer DROP COLUMN grade;
+/*------------------------------------
+Table CUSTOMER이(가) 변경되었습니다.
+-------------------------------------*/
+DESC customer;
+/*-----------------------------
+이름        널        유형           
+--------- -------- ------------ 
+USERID    NOT NULL VARCHAR2(4)  
+NAME      NOT NULL VARCHAR2(30) 
+BIRTHYEAR          NUMBER(4)    
+REGDATE            DATE         
+ADDRESS            VARCHAR2(30) 
+PHONE              VARCHAR2(11)
+--------------------------------*/
+ALTER TABLE customer ADD
+(  grade VARCHAR2(30)
+ , CONSTRAINT ck_customer_grade CHECK (grade IN ('VIP', 'GOLD', 'SILVER'))
+);
+/*----------------------------------
+Table CUSTOMER이(가) 변경되었습니다.
+------------------------------------*/
+DESC customer;
+/*-----------------------------
+이름        널        유형           
+--------- -------- ------------ 
+USERID    NOT NULL VARCHAR2(4)  
+NAME      NOT NULL VARCHAR2(30) 
+BIRTHYEAR          NUMBER(4)    
+REGDATE            DATE         
+ADDRESS            VARCHAR2(30) 
+PHONE              VARCHAR2(11) 
+GRADE              VARCHAR2(30)
+------------------------------*/
+
+-- 9)
+ALTER TABLE customer MODIFY phone VARCHAR2(4);
+-- Table CUSTOMER이(가) 변경되었습니다.
+DESC customer;
+/*-----------------------------
+이름        널        유형           
+--------- -------- ------------ 
+USERID    NOT NULL VARCHAR2(4)  
+NAME      NOT NULL VARCHAR2(30) 
+BIRTHYEAR          NUMBER(4)    
+REGDATE            DATE         
+ADDRESS            VARCHAR2(30) 
+PHONE              VARCHAR2(4)  
+GRADE              VARCHAR2(30)
+-------------------------------*/
+ALTER TABLE customer MODIFY userid VARCHAR2(30);
+-- Table CUSTOMER이(가) 변경되었습니다.
+DESC customer;
+/*-----------------------------
+이름        널        유형           
+--------- -------- ------------ 
+USERID    NOT NULL VARCHAR2(30) 
+NAME      NOT NULL VARCHAR2(30) 
+BIRTHYEAR          NUMBER(4)    
+REGDATE            DATE         
+ADDRESS            VARCHAR2(30) 
+PHONE              VARCHAR2(4)  
+GRADE              VARCHAR2(30)
+-------------------------------*/
+ALTER TABLE customer MODIFY userid NUMBER(4);
+-- Table CUSTOMER이(가) 변경되었습니다.
+DESC customer;
+/*-----------------------------
+이름        널        유형           
+--------- -------- ------------ 
+USERID    NOT NULL NUMBER(4)    
+NAME      NOT NULL VARCHAR2(30) 
+BIRTHYEAR          NUMBER(4)    
+REGDATE            DATE         
+ADDRESS            VARCHAR2(30) 
+PHONE              VARCHAR2(4)  
+GRADE              VARCHAR2(30)
+------------------------------*/
+ALTER TABLE customer MODIFY userid VARCHAR2(30);
+-- Table CUSTOMER이(가) 변경되었습니다.
+DESC customer;
+/*-----------------------------
+이름        널        유형           
+--------- -------- ------------ 
+USERID    NOT NULL VARCHAR2(30) 
+NAME      NOT NULL VARCHAR2(30) 
+BIRTHYEAR          NUMBER(4)    
+REGDATE            DATE         
+ADDRESS            VARCHAR2(30) 
+PHONE              VARCHAR2(4)  
+GRADE              VARCHAR2(30) 
+-------------------------------*/
